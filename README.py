35cdbb12df6b27d37252bc1685695f90bf4c6117
alolqwerty <158836406+alolqwerty@users.noreply.github.com>
2024-10-13 13:15:14 +0300

    code for phonebook has been added

---
+++ README.py
@@ -0,0 +1,9 @@
+os import Text.py as t
+a = str(input("name or phone))
+for i in t:
+    if a == i[name] or i[num]:
+        print(i)
+    else:
+        print("not matching data")
+return a, i
+      
